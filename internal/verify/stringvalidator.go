310785b830daee18d149383024525de0a89c67db
dusdjhyeon <dusdj0813@gmail.com>
2024-08-27 18:32:03 +0900

    feat: add validators

--- internal/verify/stringvalidator.go
+++ internal/verify/stringvalidator.go
@@ -3,8 +3,6 @@ package verify
 import (
 	"fmt"
 	"regexp"
-	"context"
-	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 
@@ -33,37 +31,4 @@ func ValidateEmptyStringElement(i []interface{}) error {
 		}
 	}
 	return nil
-}
-
-// Check that the attribute does not contain a specific string 
-type notContainValidator struct {
-    target string
-}
-
-func NotContain(target string) validator.String {
-    return notContainValidator{
-        target: target,
-    }
-}
-
-func (v notContainValidator) Description(ctx context.Context) string {
-    return fmt.Sprintf("Value must not contain the string '%s'.", v.target)
-}
-
-func (v notContainValidator) MarkdownDescription(ctx context.Context) string {
-    return v.Description(ctx)
-}
-
-func (v notContainValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
-    if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
-        return
-    }
-
-    if strings.Contains(req.ConfigValue.ValueString(), v.target) {
-        resp.Diagnostics.AddAttributeError(
-            req.Path,
-            "Invalid Value",
-            fmt.Sprintf("Value must not contain the string '%s'.", v.target),
-        )
-    }
 }
--- internal/verify/verifystring/not_contain_test.go
+++ internal/verify/verifystring/not_contain_test.go
@@ -1,14 +1,13 @@
-package verify
+package verifystring_test
 
 import (
 	"context"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/terraform-providers/terraform-provider-ncloud/internal/verify"
+	"github.com/terraform-providers/terraform-provider-ncloud/internal/verify/verifystring"
 )
 
 func TestContainValidator(t *testing.T) {
@@ -22,28 +21,28 @@ func TestContainValidator(t *testing.T) {
 
 	testCases := map[string]testCase{
 		"contains-substring": {
-			in: types.StringValue("hello123"),
-			validator: verify.NotContain("hello"),
+			in:        types.StringValue("hello123"),
+			validator: verifystring.NotContain("hello"),
 			expErrors: 1,
 		},
 		"does-not-contain-substring": {
-			in: types.StringValue("goodbye"),
-			validator: verify.NotContain("hello"),
+			in:        types.StringValue("goodbye"),
+			validator: verifystring.NotContain("hello"),
 			expErrors: 0,
 		},
 		"confirm-other-attr": {
-			in: types.StringValue("gooyehi99993"),
-			validator: verify.NotContain(path.MatchRoot("does-not-contain-substring").String()),
+			in:        types.StringValue("gooyehi99993"),
+			validator: verifystring.NotContain(path.MatchRoot("does-not-contain-substring").String()),
 			expErrors: 0,
 		},
 		"skip-validation-on-null": {
-			in: types.StringNull(),
-			validator: verify.NotContain("hello"),
+			in:        types.StringNull(),
+			validator: verifystring.NotContain("hello"),
 			expErrors: 0,
 		},
 		"skip-validation-on-unknown": {
-			in: types.StringUnknown(),
-			validator: verify.NotContain("hello"),
+			in:        types.StringUnknown(),
+			validator: verifystring.NotContain("hello"),
 			expErrors: 0,
 		},
 	}
@@ -68,37 +67,6 @@ func TestContainValidator(t *testing.T) {
 
 			if test.expErrors == 0 && res.Diagnostics.HasError() {
 				t.Fatalf("expected no error(s), got %d: %v", res.Diagnostics.ErrorsCount(), res.Diagnostics)
-			}
-		})
-	}
-}
-
-func TestContainValidator_Description(t *testing.T) {
-	t.Parallel()
-
-	type testCase struct {
-		target   string
-		expected string
-	}
-
-	testCases := map[string]testCase{
-		"simple-description": {
-			target:   "foo",
-			expected: "Value must not contain the string 'foo'.",
-		},
-	}
-
-	for name, test := range testCases {
-		name, test := name, test
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			v := verify.NotContain(test.target)
-
-			got := v.MarkdownDescription(context.Background())
-
-			if diff := cmp.Diff(got, test.expected); diff != "" {
-				t.Errorf("unexpected difference: %s", diff)
 			}
 		})
 	}
